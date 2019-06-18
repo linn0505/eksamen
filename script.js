1c381bcf3e94ba3c3cd28e2a9440eaa41970a17e
Linn Wollesen <linnwollesen@Linns-MacBook-Pro.local>
2019-06-19 00:59:32 +0200

    final - js mm

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+window.addEventListener("load", sidenVises)
+
+//Når siden er loaded kald functionen sidenVises
+
+function sidenVises() {
+    //Udskriv at du er i denne funtion i consolen
+    console.log("sidenVises");
+    //Kald loadSide functionen
+    loadSide();
+}
+
+function loadSide() {
+    //Udskriv at du er i denne funtion i consolen
+    console.log("loadSide");
+    //Gør så man kan klikke på #man vælger
+    document.querySelector("#tlf").addEventListener("click", clickRotation);
+}
+
+function clickRotation() {
+    //Udskriv at du er i denne funtion i consolen
+    console.log("clickRotation");
+    //tilføj classen forsvind til this
+    this.classList.add("rotation");
+    this.removeEventListener("click", clickRotation);
+}
+
+
 
